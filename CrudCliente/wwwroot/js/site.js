64351bd19165c0d4b6075d6ad6e9ea85751795d4
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2024-02-22 16:29:51 -0300

    Estilizando

--- CrudCliente/wwwroot/js/site.js
+++ CrudCliente/wwwroot/js/site.js
@@ -2,4 +2,32 @@
     language: {
         url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
     },
+    "ordering": true,
+    "paging": true,
+    "pagingType":"simple_numbers",
+    "searching": true,
+    "oLanguage": {
+        "sEmptyTable": "Nenhum registro encontrado na tabela",
+        "sInfo": "Página de _START_ a _END_ registros (Total:‎ _TOTAL_ registros) ",
+        "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+        "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "Por página: _MENU_‎‎",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sSearch": "",
+        "oPaginate": {
+            "sNext": "Proximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Ultimo"
+        },
+        "oAria": {
+            "sSortAscending": ": Ordenar colunas de forma ascendente",
+            "sSortDescending": ": Ordenar colunas de forma descendente"
+        }
+    }
 });
+    
